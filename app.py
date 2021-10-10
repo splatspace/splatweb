93b724f4e0a37584fe546feb76005d9de037b6aa
Blake McNeill <blakemcneill@gmail.com>
2021-10-10 10:27:31 -0400

    Converting everything to Flask

--- app.py
+++ app.py
@@ -7,6 +7,38 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+@app.route("/membership")
+def membership():
+    return render_template("membership.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/contributors")
+def contributors():
+    return render_template("contributors.html")
+
+@app.route("/currentmembers")
+def currentmembers():
+    return render_template("currentmembers.html")
+
+@app.route("/donate")
+def donate():
+    return render_template("donate.html")
+
+@app.route("/equipment")
+def equipment():
+    return render_template("equipment.html")
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('404.html'), 404
