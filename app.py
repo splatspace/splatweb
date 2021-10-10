f666cb7d244935ec572719018ad2bd165078f991
Blake McNeill <blakemcneill@gmail.com>
2021-10-09 21:08:26 -0400

    Copied over files from other web

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask import render_template, url_for
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "howdy"
+def index():
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
